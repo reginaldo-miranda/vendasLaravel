bab0d84001deb8d51b50d78080e7039bb0d924fb
reginaldo <reginaldobrain@gmail.com>
2020-12-08 16:52:16 -0300

    firma 08122020 1652

--- resources/views/menus/menu.blade.php
+++ resources/views/menus/menu.blade.php
@@ -17,3 +17,33 @@
  </nav>
 
 </div>
+<nav id=menu_submenu>
+    <ul>
+        <li><a href="#">Cadastros</a>
+            <ul>
+                <li>
+                    <a href="FRcadastro.php">Clientes</a>
+                    <a href="FRcadastroPlano.php">Planos</a>
+                </li>
+            </ul>
+        <li>
+        <li><a href="#">listagem</a>
+            <ul>
+                <li>
+                    <a href="FRlistaPesquisa_cadastro.php">Clientes</a>
+                    <a href="FRlistaPesquisa_plano.php">Planos</a>
+                </li>
+            </ul>
+        </li>
+        <li><a href="#">Relatorios</a>
+            <ul>
+                <li>
+                    <a href="FRlistaPesquisaMovimento.php">Movimentos</a>
+                </li>
+            </ul>
+        </li>
+        <li><a href="#">a desemvolver</a></li>
+        <li><a href="#">a desemvolver<</a> <li>
+        <li><a href="FRlistaALiberar.php">Liberar acesso</a></li>
+    </ul>
+</nav>
